378bcb16d7e137bdc56d2c1248bf0f08301d9e3b
ysj <ysj17366781>
2015-12-11 14:53:30 +0800

    ttt

--- src/com/test/Crawer.java
+++ src/com/test/Crawer.java
@@ -10,7 +10,6 @@ package com.test;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.safety.Whitelist;
 import org.jsoup.select.Elements;
 
 import com.test.model.Fund;
